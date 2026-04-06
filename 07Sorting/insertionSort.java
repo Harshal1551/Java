c6f561728813839b7ecee669cd85305ca97883ae
Harshal Palve <harshalpalve1551@gmail.com>
2026-04-06 21:05:20 +0530

    Sorting

--- 07Sorting/insertionSort.java
+++ 07Sorting/insertionSort.java
@@ -1,32 +1,51 @@
+
+import java.util.Scanner;
+
  
 public class insertionSort {
 
-    public static void insertion(int arr[]){
-         for(int i=1; i<arr.length; i++){
-            int curr = arr[i];
-            int prev = i-1;
+   public static void insertion(int arr[]) {
+       for(int i=1; i<arr.length; i++){
+            int key =arr[i];  
+            int j = i-1;
 
-            // find position to insert 
-            while (prev>=0 && arr[prev]>curr) { 
-                arr[prev+1] = arr[prev];
-                prev--;
+            while(j>=0 && arr[j]>key){
+                arr[j+1] = arr[j];
+                j--;
             }
-            // insertion
-            arr[prev+1] = curr;
-         }
-    }
-    
-     public static void printArr(int arr[]) {
-        for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i]+ " ");
-        }
-        System.out.println();
-    }
+            arr[j+1] = key;
+       }
+
+   }
+
+
     
 
     public static void main(String[] args) {
-        int arr[] = {5,4,1,3,2};
-        insertion(arr);
-        printArr(arr);
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Enter array size: ");
+       int n = sc.nextInt();
+
+       int arr[] = new int[n];
+       System.out.println("Enter elements: ");
+       for(int i=0; i<n; i++){
+        arr[i] = sc.nextInt(); 
+       }
+
+       System.out.println("Array: ");
+       for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+       }
+
+       System.out.println();
+       insertion(arr);
+
+
+       System.out.println("Sorted Array: ");
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+
+
     }
 }
