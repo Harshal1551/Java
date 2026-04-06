36c7dfae2228d71acd86217f1d392faed548ed4a
Harshal Palve <harshalpalve1551@gmail.com>
2026-04-06 21:52:44 +0530

    Searching

--- 06Arrays/binarySearch.java
+++ 06Arrays/binarySearch.java
@@ -1,30 +1,58 @@
 
+import java.util.Scanner;
+
 public class binarySearch {
     
-    public static int binSearch(int numbers[], int key){
-        int start = 0;
-        int end = numbers.length-1;
+    public static int binSearch(int arr[], int key){
+        int start = 0 , end = arr.length-1;
 
-        while (start <= end) { 
-            int mid = (start+end)/2;
+        while(start <= end){
+            int mid = (start + end) /2;
 
-            if(numbers[mid]==key){
+            if(arr[mid] == key){
                 return mid;
-            } 
-
-            if(numbers[mid] < key){
-                start = mid+1;
-            } else {
+            }
+            else if (arr[mid]<key) {
+                    start = mid+1;                
+            }
+            else {
                 end = mid-1;
             }
         }
         return -1;
     }
+
+
+
     public static void main(String[] args) {
-        int numbers[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
-        int key= 8;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
 
-       System.out.println("Index for the " + key +" is: "+  binSearch(numbers, key));
+        int arr[]= new int[n];
+        System.out.println("Enter elements: ");
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
 
+        System.out.println("Array: ");
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+
+        System.out.println();
+
+        System.out.println("Enter a key for find: ");
+        int key = sc.nextInt();
+
+        int result = binSearch(arr, key);
+
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found"); 
+        }
+
+        
     }
 }
